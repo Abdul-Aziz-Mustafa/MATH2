e4aeaa6db6de3928e4a934d129e54f906f87f3a9
Abdul Aziz <abdul20485@iiit.ac.in>
2021-09-28 12:44:16 +0530

    hi

--- prob1.m
+++ prob1.m
@@ -2,7 +2,7 @@ function a=prob1(n) % creating a function
 if n==40 || n==50 || n==70 %given in the question
     a=0.025;%
 
-elseif n==80 || n==90 || n==100 || n==110 %given  in the qusetion 
+elseif n==80 || n==90 || n==100 || n==110 %given  in the qusetion lol
     a=0.225;
    
 else %given in the question
